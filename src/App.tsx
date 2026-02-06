63c774a62c1100e169e39e2d825d125ff2025f99
Manoj Pethe <manoj.pethe@gmail.com>
2026-02-06 17:38:48 +0530

    routing fixed using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { HashRouter } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import Navbar from './Components/Navigation/Navbar.tsx';
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <div>
-      <button className="btn"> All right! </button>
-      </div>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Landing page....</div>} />
+        <Route path="/home" element={<div><Navbar /><Outlet /></div>}>
+          <Route path="projects" element={<div>Projetcs</div>} />
+          <Route path="settings" element={<div>Settings</div>} />
+        </Route>
+      </Routes>
+    </HashRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.tsx'
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/home", element: <div>Hello World!</div> },
-]);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
