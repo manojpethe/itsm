3dac03056de86f1ad38f008227dbaa057ec7d17d
Manoj Pethe <manoj.pethe@gmail.com>
2026-02-06 12:48:47 +0530

    react router added to codebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/home", element: <div>Hello World!</div> },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
