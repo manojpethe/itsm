e11da286435d3ad86c7de90459d3c4f113a199e7
Manoj Pethe <manoj.pethe@gmail.com>
2026-02-10 16:59:16 +0530

    icon store update

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,6 +1,16 @@
+import { StateContext } from "../state/StateContext"
+import { useContext } from "react";
+
 const Settings = () => {
+  const projects = useContext(StateContext).projects;
+  projects.push({id:1,title:"title",info:"Info"})
+    
+  
   return (
+    <div>
     <div>Settings page</div>
+    <pre>{JSON.stringify(projects,null, 2)}</pre>
+    </div>
   )
 }
 
